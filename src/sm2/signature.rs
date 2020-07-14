d572de3d62e9b29f065d0be88afaec8861794798
HaoXuan40404 <444649358@qq.com>
2020-07-14 19:28:02 +0800

    update hex decode

--- src/sm2/signature.rs
+++ src/sm2/signature.rs
@@ -25,6 +25,7 @@ use byteorder::{BigEndian, WriteBytesExt};
 pub type Pubkey = Point;
 pub type Seckey = BigUint;
 
+#[derive(Clone, Debug)]
 pub struct Signature {
     r: BigUint,
     s: BigUint,
@@ -35,6 +36,18 @@ impl Signature {
         let r = BigUint::from_bytes_be(r_bytes);
         let s = BigUint::from_bytes_be(s_bytes);
         Signature { r, s }
+    }
+
+    pub fn hex_encode(self) -> (String, String) {
+        let r = hex::encode(self.r.to_bytes_be());
+        let s = hex::encode(self.s.to_bytes_be());
+        (r, s)
+    }
+
+    pub fn hex_decode(r: String, s: String) -> Signature {
+        let r = BigUint::from_bytes_be(&hex::decode(r).unwrap());
+        let s = BigUint::from_bytes_be(&hex::decode(s).unwrap());
+        Signature {r, s}
     }
 
     pub fn der_decode(buf: &[u8]) -> Result<Signature, yasna::ASN1Error> {
@@ -149,7 +162,8 @@ impl SigCtx {
 
     pub fn sign(&self, msg: &[u8], sk: &BigUint, pk: &Point) -> Signature {
         // Get the value "e", which is the hash of message and ID, EC parameters and public key
-        let digest = self.hash("1234567812345678", pk, msg);
+    //    let digest = self.hash("1234567812345678", pk, msg);
+        let digest = self.hash("31323334353637383132333435363738", pk, msg);
 
         self.sign_raw(&digest[..], sk)
     }
@@ -198,7 +212,8 @@ impl SigCtx {
 
     pub fn verify(&self, msg: &[u8], pk: &Point, sig: &Signature) -> bool {
         //Get hash value
-        let digest = self.hash("1234567812345678", pk, msg);
+       let digest = self.hash("1234567812345678", pk, msg);
+        // let digest = self.hash("31323334353637383132333435363738", pk, msg);
         //println!("digest: {:?}", digest);
         self.verify_raw(&digest[..], pk, sig)
     }
@@ -298,15 +313,37 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_sm2_for_dxct() {
+        let ctx = SigCtx::new();
+        let private_key_vec = hex::decode("D96C222D8602B287973E2ACA7E3FEDADFD0BD67F2914D3E16F46FAB8A8506F2B").unwrap();
+        let new_sk = ctx.load_seckey(&private_key_vec[..]).unwrap();
+        let pk = ctx.pk_from_sk(&new_sk);
+        let pk_raw = ctx.serialize_pubkey(&pk, false);
+        let pk_str = hex::encode(pk_raw);
+        assert_eq!(&pk_str, "04faa556408d8dbfbe6167a15869e7b4c48bf04f5ee724faa640bab1d0b9eb86f3b2c3cdb628da1144fc67d2f7b25a42906dc5b5f337c01ed89be635f211f58d27");
+        let msg = hex::decode("00").unwrap();
+        let signature = ctx.sign(&msg, &new_sk, &pk);
+        println!("signature = {:?}", signature.hex_encode());
+    }
+
+    #[test]
     fn test_sign_and_verify() {
-        let string = String::from("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd");
-        let msg = string.as_bytes();
+        let string = String::from("2daef60e7a0b8f5e024c81cd2ab3109f2b4f155cf83adeb2ae5532f74a157fdf");
+        let msg = hex::decode(string).unwrap();
+
 
         let ctx = SigCtx::new();
         let (pk, sk) = ctx.new_keypair();
-        let signature = ctx.sign(msg, &sk, &pk);
+        let pk_raw = ctx.serialize_pubkey(&pk, false);
+        println!("pk = {}", hex::encode(pk_raw));
 
-        assert!(ctx.verify(msg, &pk, &signature));
+        let signature = ctx.sign(&msg, &sk, &pk);
+        let (r, s) = signature.hex_encode();
+        println!("sig_hex r = {}, s = {}", r, s);
+        let sig_decode = Signature::hex_decode(r, s);
+        println!("sig_decode = {:?}", sig_decode);
+
+//        assert!(ctx.verify(&msg, &pk, &signature));
     }
 
     #[test]
@@ -349,6 +386,8 @@ mod tests {
             0xe4, 0xe2, 0x41, 0x67, 0xc4, 0x87, 0x5c, 0xf2, 0xf7, 0xa2, 0x29, 0x7d, 0xa0, 0x2b,
             0x8f, 0x4b, 0xa8, 0xe0,
         ];
+        println!("msg = {}", hex::encode(msg));
+
 
         let pk: &[u8] = &[
             4, 233, 185, 71, 125, 111, 174, 63, 105, 217, 19, 218, 72, 114, 185, 96, 243, 176, 1,
@@ -356,6 +395,8 @@ mod tests {
             191, 116, 238, 240, 197, 158, 1, 145, 177, 107, 112, 91, 101, 86, 50, 204, 218, 254,
             172, 2, 250, 33, 56, 176, 121, 16, 215,
         ];
+        println!("pk = {}", hex::encode(pk));
+
 
         let sig: &[u8] = &[
             48, 69, 2, 33, 0, 171, 111, 172, 181, 242, 159, 198, 106, 33, 229, 104, 147, 245, 97,
@@ -370,7 +411,68 @@ mod tests {
         let pk = curve.bytes_to_point(&pk).unwrap();
 
         let sig = Signature::der_decode(&sig).unwrap();
-
+        println!("sig = {:?}", sig);
         assert!(ctx.verify_raw(msg, &pk, &sig));
+
+        let (r, s) = sig.hex_encode();
+        println!("sig_hex r = {}, s = {}", r, s);
+        let sig_decode = Signature::hex_decode(r, s);
+        println!("sig_decode = {:?}", sig_decode);
+        println!("{}", ctx.verify_raw(msg, &pk, &sig_decode));
+
+       let r = BigUint::from_bytes_be(&hex::decode("FA30BA6D44A9CA88FDBA5EF153C86605DAB9C24B44E1804FC802E73B81D04FE9").unwrap());
+       let s = BigUint::from_bytes_be(&hex::decode("B09D1335ED0CA9A3ECF20607789FC1DD9EBA5ECF5C65F7C916863629336794D4").unwrap());
+       let signature = Signature{r, s};
+       println!("signature = {:?}", signature);
+       let pk_test = hex::decode("04FAA556408D8DBFBE6167A15869E7B4C48BF04F5EE724FAA640BAB1D0B9EB86F3B2C3CDB628DA1144FC67D2F7B25A42906DC5B5F337C01ED89BE635F211F58D27").unwrap();
+       let pk = curve.bytes_to_point(&pk_test).unwrap();
+       let msg = hex::decode("44DB476208775A0E5DBD7C00D08833A7083E232DFA95788E2EC7CC231772C23A").unwrap();
+       println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify(&msg, &pk, &signature));
+
+       let r = BigUint::from_bytes_be(&hex::decode("7dc3cc3fd443ff142b9ca42aa08a1df4a2e841416cd24eb6b7c6aad24b08bcfc").unwrap());
+       let s = BigUint::from_bytes_be(&hex::decode("decdf285cb16bf3e5cd02eeeaf99a95835721b837b359df1173289caee260be8").unwrap());
+       let signature = Signature{r, s};
+       println!("signature = {:?}", signature);
+       let pk_test = hex::decode("04f16305eee0f82ca4cf8532d48f42a6dc87f6f37d39481cd15d6b318fa6e778a85fbf09583ef46b0418810466a6ec5559cca7af2f772caca351a345a153157ee8").unwrap();
+       let pk = curve.bytes_to_point(&pk_test).unwrap();
+       let msg = hex::decode("2daef60e7a0b8f5e024c81cd2ab3109f2b4f155cf83adeb2ae5532f74a157fdf").unwrap();
+       println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify(&msg, &pk, &signature));
+
+    //    let r = BigUint::from_bytes_be(&hex::decode("84DB051DE095DE8DB339911F5EDEF2908B892451C4C3C0B78B157A436E2C3891").unwrap());
+    //    let s = BigUint::from_bytes_be(&hex::decode("8BE4754C25EE77BA5F6401FE8C283CD7BB0021748DD055792C59AB6221D9E9A4").unwrap());
+    //    let signature = Signature{r, s};
+    //    println!("signature = {:?}", signature);
+    //    let pk_test = hex::decode("04FAA556408D8DBFBE6167A15869E7B4C48BF04F5EE724FAA640BAB1D0B9EB86F3B2C3CDB628DA1144FC67D2F7B25A42906DC5B5F337C01ED89BE635F211F58D27").unwrap();
+    //    let pk = curve.bytes_to_point(&pk_test).unwrap();
+    //    let msg = hex::decode("00").unwrap();
+    //    println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify(&msg, &pk, &sig));
+
+    //    let r = BigUint::from_bytes_be(&hex::decode("AEC627694305D4334163B7EC7C2B2C8FB5BC416DEA0220094A1E08B546D793D0").unwrap());
+    //    let s = BigUint::from_bytes_be(&hex::decode("3BA3AE45CC5543FBF9154C1DDEDA922BBD77D39E78CA997225D29098560F0E47").unwrap());
+    //    let signature = Signature{r, s};
+    //    println!("signature = {:?}", signature);
+    //    let pk_test = hex::decode("04A62178C87DE5B396A6655DD26A0A87AE6B5EA7E0DA44C4E11F3BED7F0C13E970B1683C48344BB65A5132ABDF728D7546289858AAF5049B70E280B429168B5553").unwrap();
+    //    let pk = curve.bytes_to_point(&pk_test).unwrap();
+    //    let msg = hex::decode("754C37FA4AA3FC278FA65007A40FF10FF991556625DBA8B4B44133D9C44A5AC6").unwrap();
+    //    println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify(&msg, &pk, &sig));
+
+    //    let r = BigUint::from_bytes_be(&hex::decode("13B0E1C21FBBBA309C144ABFFC55455BC729BEB1505468F7E3FD7548568B0C6D").unwrap());
+    //    let s = BigUint::from_bytes_be(&hex::decode("50CA851C1488941D4AF0AFF0BADCBB8F4EEECC084DC5EF44B62BCCDCBA7B467B").unwrap());
+    //    let signature = Signature{r, s};
+    //    println!("signature = {:?}", signature);
+    //    let pk_test = hex::decode("04C6C83E3FD3CFE6509554711F68B0503612ABC45ADE80E2C9E22B39D475DAE9FCABF8C0EA1A491805A595AFF627916CEBA627B4A2DD1353DF844D648829498A09").unwrap();
+    //    let pk = curve.bytes_to_point(&pk_test).unwrap();
+    //    let msg = hex::decode("85B013F60835FE86D9D88E59EB99F4A6B0144307FB29810098320FA21455818D").unwrap();
+    //    println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify_raw(&msg, &pk, &sig));
+
+    //    let r = BigUint::from_bytes_be(&hex::decode("cb112baccd0a9de9760886dc7438992c05249edbd97524999a2f10b61ebde826").unwrap());
+    //    let s = BigUint::from_bytes_be(&hex::decode("14d290a701b029a8d33ea57af91c22f3db699fbfb7d7ac3f4a763ecf51563605").unwrap());
+    //    let signature = Signature{r, s};
+    //    println!("signature = {:?}", signature);
+    //    let pk_test = hex::decode("04f16305eee0f82ca4cf8532d48f42a6dc87f6f37d39481cd15d6b318fa6e778a85fbf09583ef46b0418810466a6ec5559cca7af2f772caca351a345a153157ee8").unwrap();
+    //    let pk = curve.bytes_to_point(&pk_test).unwrap();
+    //    let msg = hex::decode("2daef60e7a0b8f5e024c81cd2ab3109f2b4f155cf83adeb2ae5532f74a157fdf").unwrap();
+    //    println!("ctx.verify(&msg, &pk, &sig) = {:?}", ctx.verify(&msg, &pk, &sig));
+
     }
 }
